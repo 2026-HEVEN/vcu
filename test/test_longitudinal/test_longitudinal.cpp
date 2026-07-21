27da2faf526d495584e8ebdd52c44cb6a9a9cc5d
thdalsgh0220 <thdalsgh0220@gmail.com>
2026-07-21 15:59:35 +0900

    feat:test_longitudinal

--- test/test_longitudinal/test_longitudinal.cpp
+++ test/test_longitudinal/test_longitudinal.cpp
@@ -1,32 +1,6 @@
 #include <unity.h>
 #include "modules/longitudinal.h"
 
-void test_throttle_drives_positive(void) {
-    float t = longitudinal_compute({100.0f, 0.0f, 0.5f, DriveMode::Normal});
-    TEST_ASSERT_TRUE(t > 0.0f);
-}
-void test_brake_regens_negative(void) {
-    float t = longitudinal_compute({0.0f, 100.0f, 0.5f, DriveMode::Normal});
-    TEST_ASSERT_TRUE(t < 0.0f);
-}
-void test_idle_is_zero(void) {
-    TEST_ASSERT_EQUAL_FLOAT(0.0f, longitudinal_compute({0.0f, 0.0f, 0.5f, DriveMode::Normal}));
-}
-
-void setUp(void) {}
-void tearDown(void) {}
-int main(int, char **) {
-    UNITY_BEGIN();
-    RUN_TEST(test_throttle_drives_positive);
-    RUN_TEST(test_brake_regens_negative);
-    RUN_TEST(test_idle_is_zero);
-    RUN_TEST(test_regen_taper_midpoint);
-    RUN_TEST(test_regen_cutoff_high_soc);
-    RUN_TEST(test_brake_override_kills_drive);
-    RUN_TEST(test_efficiency_limits_drive);
-    return UNITY_END();
-}
-
 void test_regen_taper_midpoint(void) {
     float t = longitudinal_compute({0.0f, 100.0f, 0.925f, DriveMode::Normal});
     TEST_ASSERT_FLOAT_WITHIN(0.5f, -10.0f, t);
@@ -57,3 +31,30 @@ void test_efficiency_limits_drive(void) {
 
     TEST_ASSERT_TRUE(e < n);
 }
+
+void test_throttle_drives_positive(void) {
+    float t = longitudinal_compute({100.0f, 0.0f, 0.5f, DriveMode::Normal});
+    TEST_ASSERT_TRUE(t > 0.0f);
+}
+void test_brake_regens_negative(void) {
+    float t = longitudinal_compute({0.0f, 100.0f, 0.5f, DriveMode::Normal});
+    TEST_ASSERT_TRUE(t < 0.0f);
+}
+void test_idle_is_zero(void) {
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, longitudinal_compute({0.0f, 0.0f, 0.5f, DriveMode::Normal}));
+}
+
+void setUp(void) {}
+void tearDown(void) {}
+int main(int, char **) {
+    UNITY_BEGIN();
+    RUN_TEST(test_throttle_drives_positive);
+    RUN_TEST(test_brake_regens_negative);
+    RUN_TEST(test_idle_is_zero);
+    RUN_TEST(test_regen_taper_midpoint);
+    RUN_TEST(test_regen_cutoff_high_soc);
+    RUN_TEST(test_brake_override_kills_drive);
+    RUN_TEST(test_efficiency_limits_drive);
+    return UNITY_END();
+}
+
